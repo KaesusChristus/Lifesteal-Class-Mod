f2ee00ad6bbc0236dbd11ea7bb479bf7266c5ceb
SirMeempo <john.paul.minecraft05@gmail.com>
2026-05-30 23:17:23 +0200

    Removed some Sickles

--- obj/Debug/net8.0/LifeStealClass.AssemblyInfo.cs
+++ obj/Debug/net8.0/LifeStealClass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeStealClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c8ee2888189f6b57ea3eb24e36852c267a52cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c408edfa7c6cf3c0dea150ccd2ee600f99710f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeStealClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeStealClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
